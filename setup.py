3df42c064440afe6f68036d44a0aba9db91029c6
Amir Sadoughi <amir.sadoughi@rackspace.com>
2015-05-15 22:41:30 +0000

    ip-address-list works!

---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name="python-neutronclient-ip-address-extension",
+    version="0.1",
+    description="Adds IP address resource support to python-neutronclient",
+    long_description="",
+    author="Rackspace",
+    author_email="",
+    url="http://TBD",
+    license="Apache License, Version 2.0",
+    install_requires=["python-neutronclient"],
+    py_modules=["python_neutronclient_ip_address_extension"],
+    entry_points={
+        "neutronclient.extension": ["ip_address = python_neutronclient_ip_address_extension"]
+    }
+)
