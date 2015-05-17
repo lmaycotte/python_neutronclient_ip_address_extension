af6f912f133e04cd551863f1d430f6100d3068f9
Amir Sadoughi <amir.sadoughi@gmail.com>
2015-05-17 04:41:05 -0500

    Cleanup of setup.py

--- setup.py
+++ setup.py
@@ -3,12 +3,24 @@ import setuptools
 setuptools.setup(
     name="python-neutronclient-ip-address-extension",
     version="0.1",
-    description="Adds IP address resource support to python-neutronclient",
+    description=("Rackspace IP Address Resource Extension for python-"
+                 "neutronclient"),
     long_description="",
     author="Rackspace",
-    author_email="",
-    url="http://TBD",
+    author_email="neutron-requests@lists.rackspace.com",
+    url=("http://github.com/rackerlabs/"
+         "python_neutronclient_ip_address_extension"),
     license="Apache License, Version 2.0",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Information Technology",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Environment :: OpenStack",
+        "Programming Language :: Python"
+    ],
     install_requires=["rackspace-python-neutronclient"],
     py_modules=["python_neutronclient_ip_address_extension"],
     entry_points={
